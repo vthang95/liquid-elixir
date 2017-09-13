13d6161a647fd6b766988d46edffa0acd59420ac
Peter Arentsen <peter.arentsen@quicknet.nl>
2017-09-13 02:29:34 +0200

    WIP: preload tags

--- lib/liquid/context.ex
+++ lib/liquid/context.ex
@@ -1,6 +1,6 @@
 defmodule Liquid.Context do
   defstruct assigns: %{}, offsets: %{}, registers: %{}, presets: %{}, blocks: [],
-            extended: false, continue: false, break: false, template: nil, global_filter: nil, version: 1
+            extended: false, continue: false, break: false, template: nil, global_filter: nil, extra_tags: %{}, version: 1
 
   def registers(context, key) do
     context.registers |> Map.get(key)
