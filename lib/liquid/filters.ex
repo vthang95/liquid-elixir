66b9eb7789c677f5039c6bbe29fd5c0c7a5d5bee
vthang95 <tranvietthang95@gmail.com>
2019-08-08 11:52:00 +0700

    Divided by float

--- lib/liquid/filters.ex
+++ lib/liquid/filters.ex
@@ -136,8 +136,11 @@ defmodule Liquid.Filters do
         {_, 0} ->
           raise ArithmeticError, message: "divided by 0"
 
-        {input, number_operand} when is_integer(input) ->
+        {input, number_operand} when is_integer(input) and is_integer(number_operand) ->
           (input / number_operand) |> floor
+
+        {input, number_operand} when is_integer(input) and is_float(number_operand) ->
+          input / number_operand
 
         {input, number_operand} ->
           input / number_operand
